5072a533773dcc619069b1ca58a7019933cabf70
unknown <ladmin@MT-IND-048NB11.psi.de>
2026-05-16 19:32:19 +0530

    feat: add modulo operation to Calculator

--- src/main/java/com/demo/Calculator.java
+++ src/main/java/com/demo/Calculator.java
@@ -21,3 +21,8 @@ public class Calculator {
         return (double) a / b;
     }
 }
+    // Feature: modulo operation
+    public int modulo(int a, int b) {
+        if (b == 0) throw new ArithmeticException("Modulo by zero");
+        return a % b;
+    }
